24d3b1eb55c72b77d92e8067286a3bbf3169ed42
saurabhamrita <sk74564@gmail.com>
2025-11-20 17:06:08 +0530

    asdfg

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 resource "azurerm_resource_group" "naya_rg" {
-  name     = "naya-rg-pipeline"
+  name     = "naya-rg-pipeline1"
   location = "West Europe"
+}
+
+resource "azurerm_storage_account" "naya_stg" {
+  name                     = "nayastgpipeline20"
+  resource_group_name      = azurerm_resource_group.naya_rg.name
+  location                 = azurerm_resource_group.naya_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
